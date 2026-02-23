d2b4cf798950362d20852fdb4487ab8d021bd050
Manish Kumar <77256586+m4dd0c@users.noreply.github.com>
2026-02-23 16:53:57 +0530

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://madrich-ui.vercel.app/sitemap.xml",
+    sitemap: "https://brutalcn.vercel.app/sitemap.xml",
   };
 }
