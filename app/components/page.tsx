b992d245ab9c06d0a96b525c3c438ba3857a8c02
h4rich <harishsuthar739@gmail.com>
2026-02-24 00:15:25 +0530

    impr: auto nav to accordion

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 const Page = () => {
+  useEffect(() => {
+    window.location.href = "/components/accordion";
+  }, []);
   return <div>components</div>;
 };
 
